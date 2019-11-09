a9ae55d23b38439d0629daa27dac5c5bc42d8720
ForMuFeng <675487914@qq.com>
2019-11-09 19:22:20 +0800

    '二叉排序树生成和二叉树递归遍历'

--- src/Day20191109/TravelTree.java
+++ src/Day20191109/TravelTree.java
@@ -11,7 +11,7 @@ import static BaseUtil.ArrayUtil.TEST_ARRAY;
 public class TravelTree {
 
     //对二叉树进行遍历
-
+    //3,1,7,9,5,22
 
     //先序遍历 中 左 右
     public static void preTravel(TreeNode treeNode){
@@ -28,10 +28,40 @@ public class TravelTree {
 
 
     //中序遍历 左 中 右
+    public static void midTravel(TreeNode treeNode){
+        if(treeNode.left!=null){
+            midTravel(treeNode.left);
+        }
+        System.out.print(treeNode.val+" ");
+        if(treeNode.right!=null){
+            midTravel(treeNode.right);
+        }
+    }
+
+
+    //后序遍历 左 右 中
+    public static void lrd(TreeNode treeNode){
+        if(treeNode.left!=null){
+            lrd(treeNode.left);
+        }
+
+        if(treeNode.right!=null){
+            lrd(treeNode.right);
+        }
+
+        System.out.print(treeNode.val+" ");
+    }
 
     public static void main(String[] args) {
         TreeNode t=TreeNode.initTreeByInts(TEST_ARRAY);
+        System.out.println("先序遍历");
         preTravel(t);
+        System.out.println();
+        System.out.println("中序遍历");
+        midTravel(t);
+        System.out.println();
+        System.out.println("后续遍历");
+        lrd(t);
     }
 
 }
